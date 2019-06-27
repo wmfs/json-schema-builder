ed205e29adba8bd67478e3e80ba82e2f160b2c9a
Oliver Jukes <oliver.jukes@wmfs.net>
2019-06-27 13:17:03 +0100

    feat: Add in w2c modules

---
+++ test/w2c-test.js
@@ -0,0 +1,38 @@
+/* eslint-env mocha */
+
+const fs = require('fs')
+const path = require('path')
+const memFs = require('mem-fs')
+const editor = require('mem-fs-editor')
+const expect = require('chai').expect
+const jsonSchemaBuilder = require('../lib/json-schema-builder')
+
+const readFile = function (path) {
+  return new Promise((resolve, reject) => fs.readFile(path, 'utf8', (err, data) => {
+    if (err) reject(err)
+    else resolve(data)
+  }))
+}
+
+const convert = function (json, callback) {
+  const store = memFs.create()
+  const virtualFs = editor.create(store)
+  const jsonSchema = jsonSchemaBuilder.dslToJsonSchema(json)
+  const outputPath = path.resolve(__dirname, 'output', 'irs-workbook.json')
+  virtualFs.writeJSON(outputPath, jsonSchema)
+
+  virtualFs.commit(err => {
+    if (err) return callback(err)
+    callback(null)
+  })
+}
+
+describe('Basic builder tests', function () {
+  it('Convert some basic stuff', async () => {
+    const file = await readFile(path.resolve(__dirname, 'fixtures', 'irs-workbook.json'))
+    const json = JSON.parse(file)
+    convert(json, (err) => {
+      expect(err).to.eql(null)
+    })
+  })
+})
